5c676a732cdb6b150de5ca26cc97fde49161a8e6
Jefri42 <127638428+Jefri42@users.noreply.github.com>
2026-02-28 21:01:43 -0400

    Configuracion final de DATABASE y ALLOWED_HOSTS

--- ideas_backend/dj_database_url.py
+++ ideas_backend/dj_database_url.py
@@ -1,12 +1,16 @@
-import dj_database_url # Asegúrate de tener este import arriba
+import os
+import dj_database_url
 
+# 1. Configuración de Base de Datos "Híbrida"
 DATABASES = {
     'default': dj_database_url.config(
-        # Esto se usará en tu PC (Localhost)
-        default='mysql://root:Jefri2311.@localhost:3306/ideas_social',
+        # Render usará la variable DATABASE_URL si existe.
+        # Si no existe (en tu PC), usará tus datos de MySQL:
+        default='mysql://root:Jefri2311.@127.0.0.1:3306/ideas_social',
         conn_max_age=600
     )
 }
 
-# Esto es necesario para MySQL en Django
-DATABASES['default']['OPTIONS'] = {'charset': 'utf8mb4'}
+# 2. Configuración de Hosts permitidos
+# El '.onrender.com' permite cualquier nombre de app que elijas en Render
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
--- ideas_backend/settings.py
+++ ideas_backend/settings.py
@@ -17,19 +17,7 @@ from datetime import timedelta
 import os
 import dj_database_url
 
-# 1. Configuración de Base de Datos "Híbrida"
-DATABASES = {
-    'default': dj_database_url.config(
-        # Render usará la variable DATABASE_URL si existe.
-        # Si no existe (en tu PC), usará tus datos de MySQL:
-        default='mysql://root:Jefri2311.@127.0.0.1:3306/ideas_social',
-        conn_max_age=600
-    )
-}
 
-# 2. Configuración de Hosts permitidos
-# El '.onrender.com' permite cualquier nombre de app que elijas en Render
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 import os
 from pathlib import Path
 import os
